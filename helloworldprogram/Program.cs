c67d461fde94d0ffc2aabee2031efe5454d27ac5
soubarnika-muthu <66513309+soubarnika-muthu@users.noreply.github.com>
2021-06-26 09:43:01 +0530

    Added addition function

--- helloworldprogram/Program.cs
+++ helloworldprogram/Program.cs
@@ -6,8 +6,12 @@ namespace helloworldprogram
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum);
             Console.WriteLine("Hello World!");
-             Console.WriteLine("welcome!");
+            Console.WriteLine("welcome!");
         }
     }
 }
